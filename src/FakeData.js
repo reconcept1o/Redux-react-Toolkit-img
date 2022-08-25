975f5c3feb25e203b55a24a17362095fe4f20bb3
reconcept1o <109516230+reconcept1o@users.noreply.github.com>
2022-08-25 11:55:34 +0300

    react toolkit

---
+++ src/FakeData.js
@@ -0,0 +1,58 @@
+export const UsersData = [
+  {
+    id: 1,
+    name: "Gary",
+    username: "Neville",
+  },
+  {
+    id: 2,
+    name: "Denis",
+    username: "Irwin",
+  },
+  {
+    id: 3,
+    name: "Jaap",
+    username: "Stam",
+  },
+  {
+    id: 4,
+    name: "Andy",
+    username: "Cole",
+  },
+  {
+    id: 5,
+    name: "Tedy",
+    username: "Sheringam",
+  },
+  {
+    id: 6,
+    name: "John ",
+    username: "Curtis",
+  },
+  {
+    id: 7,
+    name: "Paul",
+    username: "Scholes",
+  },
+  {
+    id: 8,
+    name: "Mikael",
+    username: "Silvestre",
+  }, {
+    id: 8,
+    name: "Ryan",
+    username: "Giggs",
+  }, {
+    id: 8,
+    name: "Jordi",
+    username: "Cruyff",
+  }, {
+    id: 8,
+    name: "Roy",
+    username: "Keane",
+  }, {
+    id: 8,
+    name: "Dwight",
+    username: "Yorke",
+  },
+];
